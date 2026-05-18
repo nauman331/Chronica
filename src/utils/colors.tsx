8539ae7c69b43d70857b522f86bb0393fe5ff57d
Muhammad Nauman <nauman33183@gmail.com>
2026-05-18 15:58:59 +0500

    colors theme setup

---
+++ src/utils/colors.tsx
@@ -0,0 +1,6 @@
+export const yellow = '#C9A227';
+export const white = '#FEFDFA';
+export const blue = '#1E162C';
+export const lightBlue = '#3B82F6';
+export const lightGreen = '#10B981';
+export const gray = '#8C8B9C';
