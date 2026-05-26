8ab879f7507cae0fffc0aae09bc85c00beb4ce26
Muhammad Nauman <nauman33183@gmail.com>
2026-05-26 09:43:11 -0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import { Provider } from "react-redux";
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from "./src/store/store";
 
+//store setup done
 
 const App: React.FC = () => {
   return (
